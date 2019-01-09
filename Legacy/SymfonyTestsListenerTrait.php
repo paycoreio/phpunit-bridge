09c6761e7e003f27eb5579aada28009fcdf2f3c2
Pashkalab <paol.pr@gmail.com>
2019-01-09 20:05:32 +0200

    Update SymfonyTestsListenerTrait.php
    delete echo's

--- Legacy/SymfonyTestsListenerTrait.php
+++ Legacy/SymfonyTestsListenerTrait.php
@@ -125,7 +125,6 @@ class SymfonyTestsListenerTrait
         }
 
         if (-1 === $this->state) {
-            echo "Testing $suiteName\n";
             $this->state = 0;
 
             if (!class_exists('Doctrine\Common\Annotations\AnnotationRegistry', false) && class_exists('Doctrine\Common\Annotations\AnnotationRegistry')) {
@@ -143,7 +142,6 @@ class SymfonyTestsListenerTrait
                     $this->state = 2;
 
                     if (!$this->wasSkipped = require $this->skippedFile) {
-                        echo "All tests already ran successfully.\n";
                         $suite->setTests(array());
                     }
                 }
